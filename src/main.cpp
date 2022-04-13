28f45a6f9e703231248269797e14bdbca373e5cc
Koen de Vos <50488480+KdVos@users.noreply.github.com>
2022-04-13 11:53:19 +0200

    Update src/main.cpp
    Co-authored-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- src/main.cpp
+++ src/main.cpp
@@ -305,8 +305,8 @@ int main(int argc, char **argv){
             robot.pub_laser.publish(scan_msg);
 
             // Create bumper data 
-            std_msgs::Bool bump_msg_f;      // front bumper message
-            std_msgs::Bool bump_msg_r;      // rear bumper message
+            std_msgs::Bool bump_msg_f; // front bumper message
+            std_msgs::Bool bump_msg_r; // rear bumper message
             bumper.generateBumperData(world,robot,bump_msg_f,bump_msg_r);
             robot.pub_bumperF.publish(bump_msg_f);
             robot.pub_bumperR.publish(bump_msg_r);
