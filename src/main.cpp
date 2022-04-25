4a775d100c57c1e07dedd80b45ebe2c253375b1b
Koen de Vos <50488480+KdVos@users.noreply.github.com>
2022-04-25 17:45:14 +0200

    remove trailing whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -113,8 +113,7 @@ int main(int argc, char **argv){
     geo::ShapeConstPtr robot_shape(&robot_shape_cyl);
     geo::Vector3 robot_color(0, 0, 1);
     
-    std::vector<Robot*> robots;
-    
+    std::vector<Robot*> robots;    
     Id hero_id = world.addObject(geo::Pose3D::identity(), robot_shape, robot_color, robottype);
     Robot hero("hero", hero_id);
     hero.base.setDisableSpeedCap(config.disable_speedcap.value());
