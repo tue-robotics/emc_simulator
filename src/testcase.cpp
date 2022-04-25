9c5c5583afd2c5230d11079ba538c34f56cae254
Koen de Vos <k.d.vos@tue.nl>
2022-04-25 08:43:54 +0200

    Tidy up example

--- src/testcase.cpp
+++ src/testcase.cpp
@@ -15,26 +15,37 @@ int main(int argc, char **argv)
     std::string heightmap_filename;
     heightmap_filename = ros::package::getPath("emc_simulator") + "/data/heightmap_correct.pgm";
 
-    geo::Vector3 rpose;
+    geo::Vector3 rpose = (0,0,0);
     std::cout<<rpose<<std::endl;
 
     std::vector<Door> doors;
     geo::ShapePtr heightmap = createHeightMapShape(heightmap_filename, doors);
 
-    for (int i = 0; i < 100; i++)
+    bool test   =  false;
+    double dist = 0;
+
+    double dist_0 = 0.15;
+    double dist_st= 0.004;
+    int N = 100;
+
+    for (int i = 0; i < N; i++)
     {
-        double dist = 0.15 + i * 0.004;
-        bool test = heightmap->intersect(rpose,dist);
+        dist = dist_0 + i * dist_st;
+        test = heightmap->intersect(rpose,dist);
  
         if (test)
         {
-            std::cout<< "Dist " << dist <<" Collision" <<std::endl;
-        }
-        else
-        {
-            std::cout<< "Dist " << dist <<" NO Collision"<<std::endl;
+            break;
         }
     }   
+    if (test)
+    {
+        std::cout<< "Dist " << dist <<" Collision" <<std::endl;
+    }
+    else
+    {
+        std::cout<< "Dist " << dist <<"NO Collision" <<std::endl;
+    }
 
     }
     
