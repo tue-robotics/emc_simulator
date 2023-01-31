79acf6adaff908fe7c7109283bb0fdc4b6eb61b5
Koen de Vos <k.d.vos@tue.nl>
2023-01-31 13:18:36 +0100

    fix includes of tf2

--- src/virtualbase.h
+++ src/virtualbase.h
@@ -8,7 +8,7 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
 #include "nav_msgs/Odometry.h"
-#include "tf/tf.h"
+#include <tf2/LinearMath/Quaternion.h>
 
 #include "random"
 #include <cmath>
