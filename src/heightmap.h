06cef8749ae02e69a097483122eb254555e9512f
PetervDooren <19806646+PetervDooren@users.noreply.github.com>
2023-03-20 13:58:03 +0100

    make function to check initialized

--- src/heightmap.h
+++ src/heightmap.h
@@ -24,6 +24,8 @@ class MapLoader
     void getMapMetadata(nav_msgs::MapMetaData& metadataRef);
 
     void getMapImage(cv::Mat& imageRef);
+    
+    bool isInitialised(){ return initialized; };
 
     private:
     ros::NodeHandle nh;
