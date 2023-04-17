067837bd057907f07e9e1e27ce0b3d8cc28899ae
Thijs Beurskens <t.m.h.beurskens@student.tue.nl>
2023-04-17 11:51:46 +0200

    fix origin relative to rviz map

--- src/heightmap.cpp
+++ src/heightmap.cpp
@@ -241,8 +241,8 @@ geo::ShapePtr createHeightMapShape(const cv::Mat& image_tmp, double resolution, 
         return geo::ShapePtr();
     }
 
-    double origin_x = -(image.cols * resolution / 2);
-    double origin_y = -(image.rows * resolution / 2);
+    double origin_x = (-image.cols / 2.0 + 1) * resolution;
+    double origin_y = (-image.rows / 2.0 + 1) * resolution;
 
     // Find doors
     for(int y = 0; y < image.rows; ++y)
