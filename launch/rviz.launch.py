d3aa3b1d2df46a83200e49f0624e051150c7805d
Wiktor <w.bocian@student.tue.nl>
2025-03-30 16:49:43 +0200

    adjust rviz launch file to be independent of emc_simulator

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,19 +1,21 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Get the path to the RViz configuration file
-    rviz_config_path = os.path.join(get_package_share_directory('emc_simulator'), 'rviz', 'mrc.rviz')
+    # Get the directory of this launch file
+    launch_file_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Set the default path to the RViz configuration file relative to this launch file
+    default_rviz_config_path = os.path.join(launch_file_dir, '../rviz/mrc.rviz')
 
     return LaunchDescription([
         # Declare an argument to allow override of the RViz config file path
         DeclareLaunchArgument(
             'rviz_config',
-            default_value=rviz_config_path,
+            default_value=default_rviz_config_path,
             description='Full path to the RViz config file'
         ),
 
